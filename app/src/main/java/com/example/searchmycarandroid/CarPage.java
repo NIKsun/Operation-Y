e9b75dc4ee9a909a92f79aae20edfe5a35107531
NIKsun <chernik2@gmail.com>
2015-07-18 10:49:04 +0300

    Add webActivity

---
+++ app/src/main/java/com/example/searchmycarandroid/CarPage.java
@@ -0,0 +1,16 @@
+package com.example.searchmycarandroid;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * Created by Никита on 18.07.2015.
+ */
+public class CarPage extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.car_web_page);
+    }
+}
